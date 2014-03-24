0f70f7b6b1eb06c85c97cbd3bf2c52602b0560c4
r2fresh <r2fresh78@gmail.com>
2014-03-24 14:44:52 +0900

    javascript guide modify

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt){
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.initConfig({
+
+		pkg : grunt.file.readJSON('package.json'),
+
+  		concat : {
+			guide :{
+					files: {
+					'javascript/guide.md': [
+					'javascript/overview.md'
+					]
+				},
+			}
+		},
+	});
+
+	grunt.registerTask('default',['concat:guide']);
+}
